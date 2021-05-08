5ff7fcaabdfa6679b0c162d74b3a2812a743ea27
Fabian Schulze <fabian.schulze@chemster.net>
2021-05-08 14:49:22 +0200

    added data fetching

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const getNavigationProps = ()=>{
+const getNavigationProps = async ()=>{
   console.log("NAVIGATION GET CALLED")
-  return "<h1>THIS IS MY ONE TIME FETCHED NAVIGATION</h1>"
+  const res = await fetch("http://localhost:3000/navigation")
+  const data = await res.json()
+  return data.html
 }
 
 app.prepare().then(() => {
@@ -16,11 +18,15 @@ app.prepare().then(() => {
 
   // static assets
   server.get("/_next/*", handle);
+  
+  server.get("/navigation", (req, res)=>{
+    res.json({"html": "<h1>THIS IS MY ONE TIME FETCHED NAVIGATION</h1>"})
+  })
 
   server.all("*", async (req, res) => {
     const html = await app.renderToHTML(req, res, req.path, req.query);
-    const navigationHtml = getNavigationProps()
-    
+    const navigationHtml = await getNavigationProps()
+
     const finalHtml = html
                         .replace("</body>", `<script>window.navigationHtml = "${navigationHtml}";</script></body>`)
                         .replace("{{navigation-placeholder}}", navigationHtml)
